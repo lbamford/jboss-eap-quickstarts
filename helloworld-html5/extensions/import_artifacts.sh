1f9c058cdd35e3fc48d87812e8ab6496228b2699
Lee B <lbamford@infinitasweb.co.uk>
2023-03-30 18:35:08 +0100

    Update import_artifacts.sh

--- helloworld-html5/extensions/import_artifacts.sh
+++ helloworld-html5/extensions/import_artifacts.sh
@@ -1,5 +1,14 @@
 
-CODE=$(curl -sSL -w '%{http_code}' -o binary.tar.gz https://myserver.com/binary.tar.gz)
+#CODE=$(curl -sSL -w '%{http_code}' -o binary.tar.gz https://myserver.com/binary.tar.gz)
+
+echo "Custom code for binary deployment"
+# Picks up WAR_FILE_URL from buildConfig
+echo ${WAR_FILE_URL}
+echo ${WAR_FILE_NAME}
+
+CODE=$(curl -sSL -w '%{http_code}' -o ${DEPLOY_DIR}/${WAR_FILE_NAME} -O ${WAR_FILE_URL})
+#curl -o ${DEPLOY_DIR}/${WAR_FILE_NAME} -O ${WAR_FILE_URL}
+
 if [[ "$CODE" =~ ^2 ]]; then
     # Server returned 2xx response
     # do_something_with binary.tar.gz
